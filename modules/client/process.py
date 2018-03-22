2f58640222384dd18653a608d4e8809df03aa723
stumblinbear <stumblinbear@gmail.com>
2018-03-22 13:00:46 -0500

    Updates

--- modules/client/process.py
+++ modules/client/process.py
@@ -77,16 +77,19 @@ class Process:
 
         args = config.program.execute['args']
 
-        args = re.sub('(-[^-\s]+) {miner\.id}', '\\1 %s' % self.client.worker_id, args)
+        args = re.sub('{miner\.id}', '\\1 %s' % self.client.worker_id, args)
 
         if config.wallet:
-            args = re.sub('(-[^-\s]+) {user}', '\\1 %s' % config.wallet.address, args)
+            args = re.sub('{user}', '\\1 %s' % config.wallet.address, args)
 
         if config.pool.endpoint is not None:
-            args = re.sub('(-[^-\s]+) {pool\.url}', '\\1 %s' % (('stratum+tcp://' if config.pool.endpoint.stratum else '') + config.pool.endpoint.url), args)
-            args = re.sub('(-[^-\s]+) {pool\.pass}', '' if not config.pool.endpoint.password else '\\1 %s' % config.pool.endpoint.password, args)
+            args = re.sub('{pool\.url}', '\\1 %s' % (('stratum+tcp://' if config.pool.endpoint.stratum else '') + config.pool.endpoint.url), args)
+            args = re.sub('{pool\.url.host}', '\\1 %s' % (('stratum+tcp://' if config.pool.endpoint.stratum else '') + config.pool.endpoint.url.split(':')[0]), args)
+            args = re.sub('{pool\.url.port}', '\\1 %s' % config.pool.endpoint.url.split(':')[1], args)
 
-            args = re.sub('(-[^-\s]+) {user}', '' if not config.pool.endpoint.username else '\\1 %s' % config.pool.endpoint.username, args)
+            args = re.sub('{pool\.pass}', '' if not config.pool.endpoint.password else '\\1 %s' % config.pool.endpoint.password, args)
+
+            args = re.sub('{user}', '' if not config.pool.endpoint.username else '\\1 %s' % config.pool.endpoint.username, args)
 
         args = re.sub('{miner\.id}', self.client.worker_id, args)
 
