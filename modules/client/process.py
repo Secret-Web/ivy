25b1b9ffd30a1df35a72c03de8625abe6e5f5a75
stumblinbear <stumblinbear@gmail.com>
2018-03-22 13:35:18 -0500

    Updates

--- modules/client/process.py
+++ modules/client/process.py
@@ -83,17 +83,22 @@ class Process:
             args = re.sub('{user}', '%s' % config.wallet.address, args)
 
         if config.pool.endpoint is not None:
-            args = re.sub('{pool\.url}', '%s' % (('stratum+tcp://' if config.pool.endpoint.stratum else '') + config.pool.endpoint.url), args)
+            if config.pool.endpoint.url:
+                args = re.sub('{pool\.url}', '%s' % (('stratum+tcp://' if config.pool.endpoint.stratum else '') + config.pool.endpoint.url), args)
 
-            if ':' in config.pool.endpoint.url:
-                args = re.sub('{pool\.url\.host}', '%s' % (('stratum+tcp://' if config.pool.endpoint.stratum else '') + config.pool.endpoint.url.split(':')[0]), args)
-                args = re.sub('{pool\.url\.port}', '%s' % config.pool.endpoint.url.split(':')[1], args)
+                if ':' in config.pool.endpoint.url:
+                    args = re.sub('{pool\.url\.host}', '%s' % (('stratum+tcp://' if config.pool.endpoint.stratum else '') + config.pool.endpoint.url.split(':')[0]), args)
+                    args = re.sub('{pool\.url\.port}', '%s' % config.pool.endpoint.url.split(':')[1], args)
 
-            args = re.sub('{pool\.pass}', '' if not config.pool.endpoint.password else '%s' % config.pool.endpoint.password, args)
+            if config.pool.endpoint.password:
+                args = re.sub('{pool\.pass}', '' if not config.pool.endpoint.password else '%s' % config.pool.endpoint.password, args)
 
-            args = re.sub('{user}', '' if not config.pool.endpoint.username else '%s' % config.pool.endpoint.username, args)
+            if config.pool.endpoint.username:
+                args = re.sub('{user}', '' if not config.pool.endpoint.username else '%s' % config.pool.endpoint.username, args)
 
         args = re.sub('{miner\.id}', self.client.worker_id, args)
+
+        args = re.sub('\B(--?[^-\s]+) ({[^\s<]+)', '', args)
 
         args = shlex.split(args)
         args.insert(0, './' + config.program.execute['file'])
