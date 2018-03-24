2bb8e44eb8cbfba41e9e9c0197065cecd6075813
stumblinbear <stumblinbear@gmail.com>
2018-03-24 16:42:47 -0500

    Updates

--- modules/client/process.py
+++ modules/client/process.py
@@ -79,7 +79,7 @@ class Process:
         await self.gpus.setup()
         await self.gpus.apply(config.hardware, self.client.group.hardware.overclock)
 
-        self.logger.info('Starting miner...')
+        self.logger.info('Starting miner: %s' % ' '.join(args))
 
         self.process = await asyncio.create_subprocess_exec(*args, cwd=miner_dir,
                         stdin=asyncio.subprocess.DEVNULL, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
