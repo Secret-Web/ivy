31c9fb4c5613dcea3962db3418ba112aad1b559c
stumblinbear <stumblinbear@gmail.com>
2018-03-26 08:22:07 -0500

    Updates

--- modules/client/monitor/ccminer.py
+++ modules/client/monitor/ccminer.py
@@ -8,7 +8,7 @@ from . import API
 class ClaymoreAPI(API):
     async def get_stats(self, host):
         session = aiohttp.ClientSession()
-        async with session.ws_connect('ws://127.0.0.1:4068/histo') as ws:
+        async with session.ws_connect('ws://127.0.0.1:4068/histo', protocols=('text',)) as ws:
             async for msg in ws:
                 if msg.type == aiohttp.WSMsgType.TEXT:
                     print(msg.data)
