36536e905c8325e79039c8e06edab4f2b7379eba
stumblinbear <stumblinbear@gmail.com>
2018-03-27 10:56:58 -0500

    Updates

--- ivy/model/program.py
+++ ivy/model/program.py
@@ -1,4 +1,6 @@
-from ivy.model.fee import FeeConfig
+from ivy.model.pool import Pool
+from ivy.model.wallet import Wallet
+from ivy.model.program import Program
 
 
 class Program:
@@ -32,5 +34,25 @@ class Program:
         if self.install is not None: obj['install'] = self.install
 
         if self.fee is not None: obj['fee'] = self.config.as_obj()
+
+        return obj
+
+class FeeConfig:
+    def __init__(self, **kwargs):
+        self.update(**kwargs)
+
+    def update(self, **kwargs):
+        self.pool = Pool(**kwargs['pool']) if 'pool' in kwargs else None
+        self.wallet = Wallet(**kwargs['wallet']) if 'wallet' in kwargs else None
+        self.program = Program(**kwargs['program']) if 'program' in kwargs else None
+
+    def as_obj(self):
+        obj = {}
+
+        obj['name'] = 'Dev Mining'
+
+        if self.pool is not None: obj['pool'] = self.pool.as_obj()
+        if self.wallet is not None: obj['wallet'] = self.wallet.as_obj()
+        if self.program is not None: obj['program'] = self.program.as_obj()
 
         return obj
