fae671d59f71c9d813122ea3914f1c97a9a6b826
stumblinbear <stumblinbear@gmail.com>
2018-07-05 19:38:46 -0500

    fix

--- modules/client/gpu/amd.py
+++ modules/client/gpu/amd.py
@@ -18,16 +18,16 @@ class AMDAPI(API):
             await self.run_cmd('PWR', 'sudo /usr/bin/ohgodatool --set-max-power %d' % overclock.amd.pwr)
 
         if overclock.core['mhz']:
-            await self.run_cmd('CMZ', 'sudo /usr/bin/ohgodatool -i %d --core-state 0 --core-clock %d' % (i, overclock.core['mhz'])
+            await self.run_cmd('CMZ', 'sudo /usr/bin/ohgodatool -i %d --core-state 0 --core-clock %d' % (i, overclock.core['mhz']))
 
         if overclock.core['vlt']:
-            await self.run_cmd('CVT', 'sudo /usr/bin/ohgodatool -i %d --volt-state 0 --vddci %d' % (i, overclock.core['vlt'])
+            await self.run_cmd('CVT', 'sudo /usr/bin/ohgodatool -i %d --volt-state 0 --vddci %d' % (i, overclock.core['vlt']))
 
         if overclock.mem['mhz']:
-            await self.run_cmd('MMZ', 'sudo /usr/bin/ohgodatool -i %d --mem-state 0 --mem-clock %d' % (i, overclock.mem['mhz'])
+            await self.run_cmd('MMZ', 'sudo /usr/bin/ohgodatool -i %d --mem-state 0 --mem-clock %d' % (i, overclock.mem['mhz']))
 
         if overclock.mem['vlt']:
-            await self.run_cmd('MVT', 'sudo /usr/bin/ohgodatool -i %d --volt-state 0 --mvdd %d' % (i, overclock.mem['vlt'])
+            await self.run_cmd('MVT', 'sudo /usr/bin/ohgodatool -i %d --volt-state 0 --mvdd %d' % (i, overclock.mem['vlt']))
 
         if overclock.fan['min']:
             await self.run_cmd('FAN', 'sudo /usr/bin/ohgodatool -i %d --set-fan-speed %d' % (i, overclock.fan['min']))
