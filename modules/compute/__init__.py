c4a4ca3faf0bbc324b3449adadaa680bf2ee4b06
stumblinbear <stumblinbear@gmail.com>
2018-07-28 13:17:51 -0500

    jh

--- modules/compute/__init__.py
+++ modules/compute/__init__.py
@@ -294,6 +294,8 @@ class ComputeModule(Module):
 
                 client.update(**data)
 
+                # TODO: Watchdog for client gpu counts
+
                 # If the group no longer exists, set it back to default
                 if not await self.database.groups.has(client.config.group.id):
                     client.config.group.reset()
